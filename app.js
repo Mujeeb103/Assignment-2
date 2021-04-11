7d5da8727382bf6455cb25e414e22b68e24d54b8
Mujeeb Choudhry <mujeeb9019@gmail.com>
2021-04-11 00:10:13 -0700

    These are the function plans for part 1 of assignment 2

---
+++ app.js
@@ -0,0 +1,14 @@
+/* function to add a new note
+function expression - may need to review reading material on how to make it
+function return- most likely need a loop for double press? - google
+console log statement*/
+
+
+/*function to save note
+create function - review reading material for save aspect
+function return - no loop since only 1 objective
+console log statement*/
+
+/*function to delete note
+create function- I think using the remove element statment? - check reading meterial
+console log statement*/
