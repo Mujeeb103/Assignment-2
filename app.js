b96e25964ebddc0f1a9eec3e383a168fe3792a46
Mujeeb Choudhry <mujeeb9019@gmail.com>
2021-04-11 22:46:59 -0700

    define note writing section function

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-/* function to add a new note
-function expression - may need to review reading material on how to make it
-function return- most likely need a loop for double press? - google
-console log statement*/
+const notesArray = []
 
-
-/*function to save note
-create function - review reading material for save aspect
-function return - no loop since only 1 objective
-console log statement*/
-
-/*function to delete note
-create function- I think using the remove element statment? - check reading meterial
-console log statement*/
+function defineNoteWritingSection() {
+    html = `
+    <div id="note-area">
+    <div id="note" contenteditable="true"></div>
+    <button onclick="saveNote()">save</button>
+    <button onclick="cleanUp()">cancel</button>
+    </div>
+    ` 
+    return html
+}
